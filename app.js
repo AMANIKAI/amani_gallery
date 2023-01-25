92cf3fd163f1b180407fb88ce3a089a97b4097fe
amani <akai@safaricom.co.ke>
2023-01-25 13:10:43 +0300

    updated app

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-
 // Import express package
 const express = require('express');
+const upload = require('./upload');
+
 
 // Initialize express
 const app = express();
 
-// route for the index page
-app.get('/', (req, res)=>{
-   response.send('<h1> Welcome to my app</h1>')
-});
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+
+// Set a static folder
+app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>{
+   res.render('index');
+})
+
+// route to handle image upload
+app.post('/upload', (req,res)=>{
+   upload(req,res, (err)=>{
+       if (err){
+           console.log(err)
+       }else{
+           console.log(req.file)
+           res.send('test');
+       }
+   })
+})
+
 
 // Define the port number
-const PORT = 8000;
+const PORT = 5000;
 
 app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
-})
+});
